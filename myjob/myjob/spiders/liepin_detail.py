265b812bf289c7db9e7722290d16aa13858f6649
徐文学 <xuwenxue@corp.autohome.inc>
2017-11-01 18:49:48 +0800

    test

--- myjob/myjob/spiders/liepin_detail.py
+++ myjob/myjob/spiders/liepin_detail.py
@@ -1,15 +1,15 @@
 import scrapy
 import configparser
 import os
-import time
 
-class LiepinSpider(scrapy.spiders.Spider):
-    name = "liepin"
+class LiepinDetailSpider(scrapy.spiders.Spider):
+    name = "liepin_detail"
     allowed_domains = ["www.liepin.com"]
     start_urls =[]
     data_source = "liepin"
     domain = "https://www.liepin.com"
-    path = "/zhaopin/"
+    data_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+    data_dir = data_dir + "/data/original/" + data_source + "/detail/"
 
     def __init__(self):
         self.start_urls = self.__init_start_urls()
@@ -18,38 +18,16 @@ class LiepinSpider(scrapy.spiders.Spider):
 
     def __init_start_urls(self):
         a = []
+        conf = configparser.ConfigParser()
+        if os.path.exists(self.data_dir):
+            files = os.listdir(self.data_dir)
+            for file_name in files:
+                conf_path = self.data_dir+file_name+"/"+file_name+".ini"
+                if os.path.exists(conf_path):
+                    conf.read(conf_path);
+                    url = conf.get("config","url")
+                    a.append(url)
 
-        params = {}
-        # default value
-        params["industries"] = ""
-        params["dqs"] = "010"
-        params["salary"] = "50$100"
-        params["jobKind"] = ""
-        params["pubTime"] = ""
-        params["compkind"] = ""
-        params["compscale"] = ""
-        params["industryType"] = ""
-        params["searchType"] = "1"
-        params["clean_condition"] = ""
-        params["isAnalysis"] = ""
-        params["init"] = "1"
-        params["sortFlag"] = "15"
-        params["flushckid"] = "0"
-        params["fromSearchBtn"] = "1"
-        params["headckid"] = "0fe19ebe8682770e"
-        params["d_headId"] = "53fff7f31621f3984070ff45e8f7eea1"
-        params["d_ckId"] = "7bf47b5b504dbb8cb9d24165dc6baa3a"
-        params["d_sfrom"] = "search_fp_nvbar"
-        params["d_curPage"] = 0
-        params["d_pageSize"] = "40"
-        params["siTag"] = "1B2M2Y8AsgTpgAmY7PhCfg~F5FSJAXvyHmQyODXqGxdVw"
-        params["key"] = "java+%E6%9E%B6%E6%9E%84%E5%B8%88"
-        for i in range(0,3):
-
-            url = self.domain+self.path+"?"
-            for k in params.keys():
-                url += "&"+k + "="+str(params[k])
-            a.append(url)
         return a
 
 ###https://www.liepin.com/zhaopin/?pubTime=&ckid=f7257d56dc3e3ccf&fromSearchBtn=2&compkind=&isAnalysis=&init=-1&searchType=1&flushckid=1&dqs=&industryType=&jobKind=&sortFlag=15&industries=dqs%3D010salary%3DjobKind%3DpubTime%3Dcompkind%3Dcompscale%3DindustryType%3DsearchType%3D1clean_condition%3DisAnalysis%3Dinit%3D1sortFlag%3D15flushckid%3D0fromSearchBtn%3D1headckid%3D0fe19ebe8682770ed_headId%3D53fff7f31621f3984070ff45e8f7eea1d_ckId%3D7bf47b5b504dbb8cb9d24165dc6baa3ad_sfrom%3Dsearch_fp_nvbard_curPage%3D0d_pageSize%3D40siTag%3D1B2M2Y8AsgTpgAmY7PhCfg~F5FSJAXvyHmQyODXqGxdVwkey%3Djava+%E6%9E%B6%E6%9E%84%E5%B8%88&salary=50$100&compscale=&key=java+%E6%9E%B6%E6%9E%84%E5%B8%88&clean_condition=&headckid=f7257d56dc3e3ccf&d_pageSize=40&siTag=8xn0k0zRebZDOrQMslcvBQ~SB9Rk-aix1mtHcCm-tdqGg&d_headId=876346d8f0a98afc194bb93502aa665f&d_ckId=876346d8f0a98afc194bb93502aa665f&d_sfrom=search_prime&d_curPage=0
@@ -59,30 +37,27 @@ class LiepinSpider(scrapy.spiders.Spider):
 #"&d_headId=53fff7f31621f3984070ff45e8f7eea1&d_ckId=7bf47b5b504dbb8cb9d24165dc6baa3a&d_sfrom=search_fp_nvbar" \
 #"&d_curPage=0&d_pageSize=40&siTag=1B2M2Y8AsgTpgAmY7PhCfg~F5FSJAXvyHmQyODXqGxdVw&key=java+%E6%9E%B6%E6%9E%84%E5%B8%88http"
     def parse(self, response):
-        dp = response.xpath('//div[@class="job-info"]/h3/a/@href').extract()
+        content = response.xpath('//div[@class="content content-word"]/text()').extract()
+
         #print(dp)
-        filename = response.url.split("/")[-2]
-        data_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-        data_dir=data_dir+"/data/original/"+self.data_source+"/detail/"
-        print(data_dir)
-        with open(filename, 'wb') as f:
+        id = response.url.split("/")[-1].replace(".shtml","")
+        print(id)
+        file_dir = self.data_dir+id
+        file_name = file_dir+"/"+id+".html"
+        if os.path.exists(file_name):
+            os.remove(file_name);
+        with open(file_name, 'wb') as f:
             f.write(response.body)
-        for p in dp:
-            if not p.startswith("http"):
-                p = self.domain + p
-            id = p.replace(self.domain,"").replace("/a/","").replace("/job/","").replace(".shtml","");
-            #print(id)
-            data_file_dir = data_dir+"/"+id
-            if not os.path.exists(data_file_dir):
-                os.makedirs(data_file_dir)
-            data_file_path = data_file_dir+"/"+id+".ini";
-            conf = configparser.ConfigParser()
-            conf.add_section('config')  # 添加section
-            conf.set('config', 'id', id)
-            conf.set('config', "url", p)
-            conf.set('config', 'domain', self.domain)
-            conf.set('config', 'last_time', time.strftime('%Y-%m-%d %H:%M:%S'))
-            with open(data_file_path, 'w') as f:
-                conf.write(f)
+
+
+        file_name_content = file_dir+"/"+id+"_content.txt"
+        if os.path.exists(file_name_content):
+            os.remove(file_name_content)
+        with open(file_name_content, 'w') as f:
+            for t in content:
+                t = t.strip()
+                t = t.replace("\n","").replace("\r","")
+                if t!='':
+                    f.write(t+"\n")
 
 
